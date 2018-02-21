b610693857fd9ebf7c2cf8eab9dca2c3c47d276a
fh123321 <fh123321@kimo.com>
2018-02-21 11:13:22 +0800

    補上一些註解
    在這部上註解

--- CommonService/DbFactory.cs
+++ CommonService/DbFactory.cs
@@ -10,6 +10,8 @@ namespace CommonService
     public class DbFactory
     {
         //這邊是DB工廠可以做一些DB加解密與產出DB的相關功能
+        //注意在Config 的 providerName 必須完整來
+        //<add name="OOXX" connectionString="XXXXXX" providerName="System.Data.SqlClient.SqlConnection, version=1.0.3300.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"/>
         public static IDbConnection CrtConnection(string connStr,string proproviderName ) {
             IDbConnection conn = (IDbConnection) System.Activator.CreateInstance(Type.GetType(proproviderName));
             conn.ConnectionString = connStr;
