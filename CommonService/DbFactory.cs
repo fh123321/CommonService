f9a34ca49ec989579245d8e9fb9f740b908c7757
fh123321 <fh123321@kimo.com>
2018-02-21 10:16:27 +0800

    部分程式代碼
    常用函式部分代碼

---
+++ CommonService/DbFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CommonService
+{
+    public class DbFactory
+    {
+        //這邊是DB工廠可以做一些DB加解密與產出DB的相關功能
+        public static IDbConnection CrtConnection(string connStr,string proproviderName ) {
+            IDbConnection conn = (IDbConnection) System.Activator.CreateInstance(Type.GetType(proproviderName));
+            conn.ConnectionString = connStr;
+            return conn;
+        }
+    }
+}
---
+++ CommonService/StaticService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace CommonService
+{
+    public class StaticService
+    {
+        public static Dictionary<Type, DbType> typeMap = new Dictionary<Type, DbType>() {
+            {typeof(byte) , DbType.Byte},
+            {typeof(sbyte) , DbType.SByte},
+            {typeof(short) , DbType.Int16},
+            {typeof(ushort) , DbType.UInt16},
+            {typeof(int) , DbType.Int32},
+            {typeof(uint) , DbType.UInt32},
+            {typeof(long) , DbType.Int64},
+            {typeof(ulong) , DbType.UInt64},
+            {typeof(float) , DbType.Single},
+            {typeof(double) , DbType.Double},
+            {typeof(decimal) , DbType.Decimal},
+            {typeof(bool) , DbType.Boolean},
+            {typeof(string) , DbType.String},
+            {typeof(char) , DbType.StringFixedLength},
+            {typeof(Guid) , DbType.Guid},
+            {typeof(DateTime) , DbType.DateTime},
+            {typeof(DateTimeOffset) , DbType.DateTimeOffset},
+            {typeof(byte[]) , DbType.Binary},
+            {typeof(byte?) , DbType.Byte},
+            {typeof(sbyte?) , DbType.SByte},
+            {typeof(short?) , DbType.Int16},
+            {typeof(ushort?) , DbType.UInt16},
+            {typeof(int?) , DbType.Int32},
+            {typeof(uint?) , DbType.UInt32},
+            {typeof(long?) , DbType.Int64},
+            {typeof(ulong?) , DbType.UInt64},
+            {typeof(float?) , DbType.Single},
+            {typeof(double?) , DbType.Double},
+            {typeof(decimal?) , DbType.Decimal},
+            {typeof(bool?) , DbType.Boolean},
+            {typeof(char?) , DbType.StringFixedLength},
+            {typeof(Guid?) , DbType.Guid},
+            {typeof(DateTime?) , DbType.DateTime},
+            {typeof(DateTimeOffset?) , DbType.DateTimeOffset},
+         //   {typeof(System.Data.Linq.Binary) , DbType.Binary}
+            };
+
+        public static string CryptographyPassword(string password, string salt)
+        {
+            string cryptographyPassword =
+                FormsAuthentication.HashPasswordForStoringInConfigFile(password + salt, "sha1");
+            return cryptographyPassword;
+        }
+        public static object GetPropValue(object src, string propName)
+        {
+            return src.GetType().GetProperty(propName).GetValue(src, null);
+        }
+        public static bool IsPropHasValue(object src, string propName) {
+            if (src == null) return false;
+            return src.GetType().GetProperty(propName).GetValue(src, null) != null;
+        }
+        public static DbType? GetDbType(Type type) {
+            if (typeMap.Where(t => t.Key == type).Count() == 1) {
+                DbType? dbType = typeMap.Where(t => t.Key == type).Select(t => t.Value).Single();
+                return dbType;
+            }
+            return null;
+        }
+    }
+}
